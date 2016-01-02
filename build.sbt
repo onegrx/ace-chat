f8e4cda65fc2e76e66cc206fd76ef2a7ca8da373
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-02 17:32:52 +0100

    Project setup and Akka import.

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "ace-chat"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % "2.4.1"
---
+++ src/main/scala/AceChat.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by onegrx on 02.01.16.
+ */
+
+import akka.actor.ActorSystem
+
+object AceChat extends App{
+
+  val system = ActorSystem("ace-chat")
+
+  println("AceChat")
+
+}
