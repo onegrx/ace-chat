155fab0496ced62a32425f3ac5c03b0a38c6084a
Bartłomiej Szałach <onegrx@gmail.com>
2016-01-02 21:08:59 +0100

    Implement case classes representing Akka communication.

---
+++ src/main/scala/Message.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by onegrx on 02.01.16.
+ */
+abstract class Message
+
+case class Connect(username: String) extends Message
+case class Broadcast(msg: String) extends Message
+case class Info(msg: String) extends Message
+case class Notice(from: String, msg: String) extends Message
